92d1485b791f04c5638021dc559eeee8ac1551ba
Yess <yesica.villalvazo@gmail.com>
2023-11-25 11:25:03 -0800

    background image

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome To Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+      title: 'Applicacion en Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/fondo.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(20.0),
-            color: const Color.fromARGB(255, 221, 220, 219),
-            child: Stack(
-              children: <Widget>[
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('Tarea 3.4 Utilización de widgets'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Text('Primer cuadro de texto'),
-                        Text('Segundo cuadri de texto'),
-                      ],
-                    ),
-                    Container(
-                      color: Colors.blue,
-                      padding: EdgeInsets.all(10.0),
-                      child: Text('Cuadro de texto en una columna'),
-                    ),
-                  ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Esta es una app movil',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
-                Positioned(
-                  right: 10,
-                  bottom: 10,
-                  child: Text('Cuadro de Texto en Stack'),
-                ),
-              ],
-            ),
+              ),
+              SizedBox(height: 20),
+              Icon(
+                Icons.favorite,
+                size: 50,
+                color: Colors.white,
+              ),
+            ],
           ),
         ),
       ),
